54f1db3d2018b5d78c8f1e43400bbb4689b176d3
akarki7 <karkiaabishkar@gmail.com>
2021-12-02 21:47:26 +0100

    Added jwt authentication in the backend

--- app/settings.py
+++ app/settings.py
@@ -91,7 +91,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "app.pagination.FinnPagination",
