bc28b1096c5236855e7d08b7a63733c0b1e3aebb
akarki7 <karkiaabishkar@gmail.com>
2021-12-05 02:09:09 +0100

    Added custom post endpoint for the properties

--- backend/properties/views.py
+++ backend/properties/views.py
@@ -1,3 +1,4 @@
+import rest_framework
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.mixins import (
     CreateModelMixin,
@@ -6,12 +7,14 @@ from rest_framework.mixins import (
     RetrieveModelMixin,
     UpdateModelMixin,
 )
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.viewsets import GenericViewSet
-
+from rest_framework import status
 from .models import Property
+from users.models import User
 
 from .serializers import PropertySerializer
-
+from rest_framework.response import Response
 from .filters import PropertyFilter
 
 
@@ -19,5 +22,23 @@ class PropertyViewSet(CreateModelMixin,
                       ListModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = PropertySerializer
-    queryset = Property.objects.all()
+    queryset = Property.objects.all().select_related("users")
     filterset_class = PropertyFilter
+
+    def create(self,request):
+        p_id=request.data['property_id']
+        u_id=request.data['users']
+        try:
+            property = Property.objects.get(property_id=p_id)
+            user=User.objects.get(id=u_id)
+            property.users.add(user)
+        except ObjectDoesNotExist:
+            property = Property.objects.create(**request.data)
+
+        out_serializer=PropertySerializer(instance=property)
+        return Response(out_serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self,request,*args,**kwargs):
+        pass
+
+
