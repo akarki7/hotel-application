838b42344137bde2d291025e42188baa76687d06
akarki7 <karkiaabishkar@gmail.com>
2021-12-05 01:02:46 +0100

    Fixed problems with tests logic

--- backend/properties/tests/test_filters.py
+++ backend/properties/tests/test_filters.py
@@ -39,15 +39,7 @@ class TestPropertyFilter(TestCase):
         )
         
         self.preexisting_property_2.users.add(self.user_id)
-
-        self.preexisting_property_3 = Property.objects.create(
-            property_id= "95",
-            name= "montabaur am quendelberg",
-            city= "Munich",
-            country= "Germany",
-            image_url= "https://limehome.imgix.net/properties/95/2be1d260-b132-4811-9429-57eb8d004d4c.jpg",
-        )
-        self.preexisting_property_3.users.add(self.user_id_2)
+        self.preexisting_property_2.users.add(self.user_id_2)
 
         self.number_of_propertues = Property.objects.count()
         self.client = APIClient()
@@ -62,12 +54,19 @@ class TestPropertyFilter(TestCase):
         assert response.status_code == 200
         assert len(response.data)==0
 
-    def test_filter_product_id_yields_two_result(self):
+    def test_filter_product_id_yields_one_result(self):
         response= self.client.get(f"/api/properties/?property_id=95")
         assert response.status_code == 200
-        assert len(response.data)==2
-    
+        assert len(response.data)==1
+
+    def test_filter_user_id_yields_one_results(self):
+        response= self.client.get(f"/api/properties/?user_id={self.user_id_2}")
+        assert response.status_code == 200
+        assert len(response.data)==1
+
     def test_filter_user_id_yields_two_results(self):
         response= self.client.get(f"/api/properties/?user_id={self.user_id}")
         assert response.status_code == 200
         assert len(response.data)==2
+    
+ 
